aaef1ccea70d29b9a1fd8d38d4eb0f4bb17135dc
LykowkoGrin <163309187+LykowkoGrin@users.noreply.github.com>
2024-03-16 20:01:35 +0300

    Update TelegramBot.cpp

--- TelegramBot.cpp
+++ TelegramBot.cpp
@@ -1,5 +1,4 @@
 #include "TelegramBot.h"
-#include <HTTPClient.h>
 
 TelegramBot::TelegramBot(const String& bot_key, const String& chat_id, int eeprom_address) {
     this->bot_key = bot_key;
@@ -20,37 +19,27 @@ bool TelegramBot::get_new_message(String& message) {
     http.end();
     if (error) return false;
 
-    if (!doc["result"][0]["message"]["message_id"].isNull() && !doc["result"][0]["message"]["text"].isNull()) {
-        unsigned int message_id = doc["result"][0]["message"]["message_id"].as<unsigned int>();
+    if (doc["result"][0]["message"]["message_id"].isNull() || doc["result"][0]["message"]["text"].isNull()) return false;
+    unsigned int message_id = doc["result"][0]["message"]["message_id"].as<unsigned int>();
 
-        // Читаем last_message_id из EEPROM
-        unsigned int last_message_id = 0;
-        EEPROM.get(eeprom_address, last_message_id);
+    // Читаем last_message_id из EEPROM
+    unsigned int last_message_id = 0;
+    EEPROM.get(eeprom_address, last_message_id);
 
-        // Если last_message_id не равен 0 и равен message_id из документа, возвращаем false
-        Serial.print("last_message_id: ");
-        Serial.println(last_message_id);
-        Serial.print("message_id: ");
-        Serial.println(message_id);
-        Serial.print("eeprom_address: ");
-        Serial.println(eeprom_address);
+    // Если last_message_id не равен 0 и равен message_id из документа, возвращаем false
 
-
-        if (last_message_id != 0 && last_message_id == message_id) {
-            return false;
-        }
-
-        // Извлекаем текст сообщения
-        message = doc["result"][0]["message"]["text"].as<String>();
-
-        // Сохраняем message_id в EEPROM
-        EEPROM.put(eeprom_address, message_id);
-        EEPROM.commit();
-
-        return true;
+    if (last_message_id != 0 && last_message_id == message_id) {
+        return false;
     }
+    // Извлекаем текст сообщения
+    message = doc["result"][0]["message"]["text"].as<String>();
 
-    return false;
+    // Сохраняем message_id в EEPROM
+    EEPROM.put(eeprom_address, message_id);
+    EEPROM.commit();
+
+    return true;
+
 }
 
 bool TelegramBot::send_message(const String& text) {
