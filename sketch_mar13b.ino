b40e6feda4986625632e34fe0af7dc10888ea99b
LykowkoGrin <163309187+LykowkoGrin@users.noreply.github.com>
2024-03-16 20:01:03 +0300

    Update sketch_mar13b.ino

--- sketch_mar13b.ino
+++ sketch_mar13b.ino
@@ -1,21 +1,33 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "TelegramBot.h"
+#include <EEPROM.h>
 
 const char* ssid = "xxx";
 const char* password = "xxx";
 const String bot_key = "xxx";
 const String chat_id = "xxx";
-const int eeprom_address = 0; // Адрес EEPROM для хранения message_id
 
-TelegramBot bot(bot_key, chat_id, eeprom_address);
+const int telegram_eeprom_address = 0; // Адрес EEPROM для хранения message_id для TelegramBot
+const int relay_eeprom_address = telegram_eeprom_address + sizeof(unsigned long); // Адрес EEPROM для хранения состояния реле
+
+const int relayPin = 4; // Пин для управления реле
+
+TelegramBot bot(bot_key, chat_id, telegram_eeprom_address);
 
 unsigned long lastCheckTime = 0;
 const unsigned long checkInterval = 60000; // Проверяем новые сообщения каждую минуту
 
 void setup() {
     Serial.begin(115200);
-    EEPROM.begin(512); // Инициализируем EEPROM
+    EEPROM.begin(100);
+    pinMode(relayPin, OUTPUT); // Установка пина реле как выхода
+
+    // Читаем состояние реле из EEPROM и устанавливаем его
+    int relayState = EEPROM.read(relay_eeprom_address);
+    digitalWrite(relayPin, relayState);
+
+    Serial.println(relayState);
 
     WiFi.onEvent(reconnect_WiFi, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
     reconnectWiFi();
@@ -26,6 +38,17 @@ void loop() {
         String message;
         if (bot.get_new_message(message)) {
             Serial.println("New message: " + message);
+            if (message.equals("/включить")) {
+                digitalWrite(relayPin, HIGH); // Включить реле
+                EEPROM.write(relay_eeprom_address, HIGH); // Сохранить состояние в EEPROM
+                EEPROM.commit();
+                Serial.println("Relay turned ON");
+            } else if (message.equals("/выключить")) {
+                digitalWrite(relayPin, LOW); // Выключить реле
+                EEPROM.write(relay_eeprom_address, LOW); // Сохранить состояние в EEPROM
+                EEPROM.commit();
+                Serial.println("Relay turned OFF");
+            }
         }
         lastCheckTime = millis();
     }
